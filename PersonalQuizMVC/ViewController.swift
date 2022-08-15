c03086729aeda9e410d5bf0001aea6847ec21e2f
Samir <imnotajedi@icloud.com>
2022-08-15 20:31:47 +0300

    Initial Commit

---
+++ PersonalQuizMVC/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PersonalQuizMVC
+//
+//  Created by Самир Кафаров on 15.08.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
